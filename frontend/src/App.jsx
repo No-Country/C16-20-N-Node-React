6ebdc9e884fd53e02e3e4629b3062fd428a4f070
nahuellupe <nahuellupev@gmail.com>
2024-02-12 21:25:11 -0300

    modified:   frontend/src/App.jsx 	new file:   frontend/src/components/Register.jsx 	modified:   frontend/src/components/SelectionUser.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Register from './components/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/SelectionUser.jsx
+++ frontend/src/components/SelectionUser.jsx
@@ -6,15 +6,15 @@ const HomePage = () => {
             <div className="flex min-w-max">
                 {/* Cliente */}
                 <div className="mx-8">
-                    <Link to="/cliente" className="bg-color2 py-4 px-8">Ir a Cliente</Link>
+                    <Link to="/Register" className="bg-color2 py-4 px-8">Ir a Cliente</Link>
                 </div>
                 {/* Restaurante */}
                 <div className="mx-8">
-                    <Link to="/restaurante" className="bg-color2 py-4 px-8">Ir a Restaurante</Link>
+                    <Link to="/Register" className="bg-color2 py-4 px-8">Ir a Restaurante</Link>
                 </div>
                 {/* Repartidor */}
                 <div className="mx-8">
-                    <Link to="/repartidor" className="bg-color2 py-4 px-8">Ir a Repartidor</Link>
+                    <Link to="/Register" className="bg-color2 py-4 px-8">Ir a Repartidor</Link>
                 </div>
             </div>
         </div>
