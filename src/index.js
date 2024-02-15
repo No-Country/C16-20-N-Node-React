2adbe70e3d5bf80c5aeeef7f4589613121fdd89e
Carla Sarabia <128642074+carlasarabiap@users.noreply.github.com>
2024-02-15 20:27:51 -0300

    Add files via upload
    creada la función listarRestaurantesRubro(),

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/*
+ * referencias:
+ * ✔️ - Finalizado
+ * ⏳ - En proceso
+ * ❌ - No realizado
+ * ⚡ - urgente
+ */
+import express, { urlencoded } from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import Conexion from "./controllers/conexion.js";
+import routerRestaurante from "./router/routerRestaurante.js";
+dotenv.config();
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+/**
+ * ruteo al restaurante
+ */
+// ✔️ - Finalizado
+app.use("/", routerRestaurante);
+const port = process.env.PORT || 3000;
+/**
+ * ruta de acceso publico
+ * acá podria ir el login
+ */
+app.get("/", (req, res) => {
+  Conexion.conectar(); //prueba que se conecta a la base de datos
+  res.send("La aplicación está funcionando!!! 😁😁😁");
+});
+
+app.listen(port, () => {
+  console.log(`La aplicación está funcionando en http://localhost:${port}`);
+});
---
+++ src/router/routerRestaurante.js
@@ -0,0 +1,46 @@
+/*
+ * referencias:
+ * ✔️ - Finalizado
+ * ⏳ - En proceso
+ * ❌ - No realizado
+ * ⚡ - urgente
+ */
+import express from "express";
+import { listarRestaurante, listarRestaurantesPorRubro } from "../controllers/restauranteController.js";
+// import { handleError } from "../helpers/error.js";
+const routerRestaurante = express.Router();
+// ⏳ - En proceso
+// ⚡ - urgente
+routerRestaurante.get("/restaurante", async (req, res) => {
+  try {
+    const restaurante = await listarRestaurante();
+    console.log(restaurante);
+    res.status(200).json(restaurante);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//routerRestaurante.get("/restaurante/:rubro", listarRestaurantesPorRubro());
+
+routerRestaurante.get("/restaurante/:rubro", async (req, res) => {
+  try {
+    const restauranteRubro = await listarRestaurantesPorRubro();
+    console.log(restauranteRubro);
+    res.status(200).json(restauranteRubro);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+
+//TODO
+// realizar los diferentes endpoints para cada método del controlador
+// por ejemplo para editar un restaurant usar '/editar/restaurant/:id'
+// probar la solicitud get desde el navegador y los post desde ThunderClient
+
+
+
+export default routerRestaurante;
