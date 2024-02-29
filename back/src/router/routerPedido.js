1217538686483ada2ae5282260960edcaf139cab
Toloza  Leonel <leotoloza6@gmail.com>
2024-02-29 19:32:29 -0300

    cambios minimos

--- back/src/router/routerPedido.js
+++ back/src/router/routerPedido.js
@@ -7,97 +7,102 @@
  */
 
 import express from "express";
-import { 
-    listarPedidos,
-    listarPedidosPorCliente,
-    listarPedidosPorRestaurant,
-    listarPedidosPorRepartidor,
-    listarPedidoPorId,
-    crearPedido,
-    editarPedido } from "../controllers/pedidosController.js";
+import {
+  listarPedidos,
+  listarPedidosPorCliente,
+  listarPedidosPorRestaurant,
+  listarPedidosPorRepartidor,
+  listarPedidoPorId,
+  crearPedido,
+  editarPedido,
+} from "../controllers/pedidosController.js";
 import { repartidorAleatorio } from "../controllers/repartidorController.js";
 const routerPedido = express.Router();
 
 //✔️ - Finalizado
 routerPedido.get("/pedido", async (req, res) => {
-    try {
-        const pedidos = await listarPedidos();
-        console.log(pedidos);
-        res.status(200).json(pedidos);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
+  try {
+    const pedidos = await listarPedidos();
+    console.log(pedidos);
+    res.status(200).json(pedidos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 //✔️ - Finalizado
 routerPedido.get("/pedido/cliente", async (req, res) => {
-    try {
-        //const id = req.params.id;
-        const id = req.session.id;
-        const pedidoCliente = await listarPedidosPorCliente(id);
-        res.status(200).json(pedidoCliente);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    //const id = req.params.id;
+    const id = req.session.usuario.id_cliente;
+    const pedidoCliente = await listarPedidosPorCliente(id);
+    res.status(200).json(pedidoCliente);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
 });
 
 //✔️ - Finalizado
 routerPedido.get("/pedido/restaurante", async (req, res) => {
-    try {
-        //const id = req.params.id;
-        const id = req.session.id;
-        const pedidoRestaurant = await listarPedidosPorRestaurant(id);
-        res.status(200).json(pedidoRestaurant);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    //const id = req.params.id;
+    const id = req.session.id;
+    const pedidoRestaurant = await listarPedidosPorRestaurant(id);
+    res.status(200).json(pedidoRestaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
 });
 
 //✔️ - Finalizado
 routerPedido.get("/pedido/repartidor/:id", async (req, res) => {
-    try {
-        //const id = req.params.id;
-        const id = req.session.id;
-        const pedidoRepartidor = await listarPedidosPorRepartidor(id);
-        res.status(200).json(pedidoRepartidor);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    //const id = req.params.id;
+    const id = req.session.id;
+    const pedidoRepartidor = await listarPedidosPorRepartidor(id);
+    res.status(200).json(pedidoRepartidor);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
 });
 
 //⏳ - En proceso
 routerPedido.post("/pedido/registro", async (req, res) => {
-    try {
-        const idCliente = req.session.id;
-        const pedido = req.body;
-        // Obtener un repartidor aleatorio
-        const repartidor = await repartidorAleatorio();
-        const idRepartidor = repartidor.id;
-        //const idRestaurant = ...;
-        const pedidoRegistro = await crearPedido(idCliente,idRestaurant, idRepartidor, pedido);
-        res.status(201).json(pedidoRegistro);
-        
-    } catch (error) {
-            res.status(500).json({ message: "Error interno del servidor" });
-            console.log(error);
-    }
+  try {
+    const idCliente = req.session.id;
+    const pedido = req.body;
+    // Obtener un repartidor aleatorio
+    const repartidor = await repartidorAleatorio();
+    const idRepartidor = repartidor.id;
+    //const idRestaurant = ...;
+    const pedidoRegistro = await crearPedido(
+      idCliente,
+      idRestaurant,
+      idRepartidor,
+      pedido
+    );
+    res.status(201).json(pedidoRegistro);
+  } catch (error) {
+    res.status(500).json({ message: "Error interno del servidor" });
+    console.log(error);
+  }
 });
 
 //✔️ - Finalizado
 routerPedido.patch("/pedido/editar/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const pedido = req.body;
-        const pedidoEditado = await editarPedido(id, pedido);
-        console.log(pedidoEditado);
-        res.status(200).json(pedidoEditado);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    const id = req.params.id;
+    const pedido = req.body;
+    const pedidoEditado = await editarPedido(id, pedido);
+    console.log(pedidoEditado);
+    res.status(200).json(pedidoEditado);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 export default routerPedido;
--- back/src/router/routerProducto.js
+++ back/src/router/routerProducto.js
@@ -7,73 +7,79 @@
  */
 
 import express from "express";
-import { listarProductos, listarProductosPorNombre, listarProductoPorId, crearProducto, editarProducto } from "../controllers/productosController.js";
+import {
+  listarProductos,
+  listarProductosPorNombre,
+  listarProductoPorId,
+  crearProducto,
+  editarProducto,
+} from "../controllers/productosController.js";
 const routerProducto = express.Router();
 
 //✔️ - Finalizado
 routerProducto.get("/producto", async (req, res) => {
-    try {
-        const productos = await listarProductos();
-        console.log(productos);
-        res.status(200).json(productos);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
+  try {
+    const productos = await listarProductos();
+    console.log(productos);
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 //✔️ - Finalizado
 routerProducto.get("/producto/nombre/:nombre", async (req, res) => {
-    try {
-        const nombre = req.params.nombre;
-        const productoNombre = await listarProductosPorNombre(nombre);
-        res.status(200).json(productoNombre);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    const nombre = req.params.nombre;
+    const productoNombre = await listarProductosPorNombre(nombre);
+    res.status(200).json(productoNombre);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
 });
 
 //✔️ - Finalizado
 routerProducto.get("/producto/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const productoId = await listarProductoPorId(id);
-        res.status(200).json(productoId);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    const id = req.params.id;
+    const productoId = await listarProductoPorId(id);
+    res.status(200).json(productoId);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 //✔️ - Finalizado
 routerProducto.post("/producto/registro", async (req, res) => {
-    try {
-        console.log(req.body);
-        //const id = req.params.id;
-        const restaurant = req.session.id;  
-        //const restaurant = req.user;  
-        console.log(`routerProducto.js:54, ${restaurant}`); 
-        const idRestaurant = restaurant.id
-        const productoRegistro = await crearProducto(idRestaurant,req.body);
-        res.status(201).json(productoRegistro);
-    } catch (error) {
-            res.status(500).json({ message: "Error interno del servidor" });
-            console.log(error);
-    }
+  try {
+    console.log(req.body);
+    //const id = req.params.id;
+    const restaurant = req.session.id;
+    //const restaurant = req.user;
+    console.log(`routerProducto.js:54, ${restaurant}`);
+    const idRestaurant = restaurant.id;
+    const productoRegistro = await crearProducto(idRestaurant, req.body);
+    res.status(201).json(productoRegistro);
+  } catch (error) {
+    res.status(500).json({ message: "Error interno del servidor" });
+    console.log(error);
+  }
 });
 
 //✔️ - Finalizado
 routerProducto.patch("/producto/editar/:id", async (req, res) => {
-    try {
-        //const id = req.params.id;
-        const idRestaurant = req.session.id;
-        const producto = req.body;
-        const productoEditado = await editarProducto(id, producto);
-        console.log(productoEditado);
-        res.status(200).json(productoEditado);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    //const id = req.params.id;
+    const idRestaurant = req.session.id;
+    const producto = req.body;
+    const productoEditado = await editarProducto(id, producto);
+    console.log(productoEditado);
+    res.status(200).json(productoEditado);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 export default routerProducto;
