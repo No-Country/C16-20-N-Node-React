3bfc873760f727646fc59bb7e8d746dc9e30aa76
Toloza  Leonel <112735110+LeoToloza1@users.noreply.github.com>
2024-03-02 07:47:43 -0300

    Update index.js

--- back/src/index.js
+++ back/src/index.js
@@ -24,7 +24,7 @@ import passport from "passport";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true
 };
 app.use(cors(corsOptions));
