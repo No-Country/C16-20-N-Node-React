dc5eefeba80df495d79104b928526b945155a331
Toloza  Leonel <leotoloza6@gmail.com>
2024-03-02 13:54:02 -0300

    cambios

--- back/src/router/routerProducto.js
+++ back/src/router/routerProducto.js
@@ -21,7 +21,7 @@ const routerProducto = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../public/img");
+    cb(null, "../public");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
