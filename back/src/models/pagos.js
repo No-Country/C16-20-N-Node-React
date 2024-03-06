7492d686c60de7746958ad2775f0dd220891a980
Toloza  Leonel <leotoloza6@gmail.com>
2024-03-05 21:10:01 -0300

    modified

--- back/src/models/pagos.js
+++ back/src/models/pagos.js
@@ -4,23 +4,23 @@ import { DataTypes, Model } from "sequelize";
 class Pagos extends Model {}
 
 Pagos.init(
-    {
-        id_pago: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        },
-        tipo_pago: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
+  {
+    id_pago: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize: Conexion.sequelize,
-        modelName: "pagos",
-        tableName: "pagos",
-        timestamps: false,
-    }
+    tipo_pago: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: Conexion.sequelize,
+    modelName: "pagos",
+    tableName: "pago",
+    timestamps: false,
+  }
 );
 
 export default Pagos;
